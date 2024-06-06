a9066c5d23e738256b5b11c3ec87025cac6f5be4
alchemistmatt <matthew.monroe@pnnl.gov>
2024-06-06 15:22:13 -0700

    Update the batch file and example output files

--- docs/Parse_Taxonomy_File.bat
+++ docs/Parse_Taxonomy_File.bat
@@ -1,12 +1,12 @@
 @echo off
 
-echo About to process file taxonomy.rdf
+echo About to process file taxonomy_excerpt.rdf
 pause
 
 echo %date% %time%
-..\bin\RDF_Taxonomy_Converter.exe taxonomy.rdf
-..\bin\RDF_Taxonomy_Converter.exe taxonomy.rdf /O:taxonomy_info_pg.txt /Postgres
-rem ..\bin\RDF_Taxonomy_Converter.exe taxonomy.rdf /O:taxonomy_info_terms_with_rank.txt /IncludeParents:False /IncludeGrandparents:False /IncludeMnemonic:False /IncludeRank:True
-rem ..\bin\RDF_Taxonomy_Converter.exe taxonomy.rdf /O:taxonomy_info_terms_only.txt /IncludeParents:False /IncludeGrandparents:False /IncludeMnemonic:False /IncludeRank:False
+..\bin\RDF_Taxonomy_Converter.exe taxonomy_excerpt.rdf
+..\bin\RDF_Taxonomy_Converter.exe taxonomy_excerpt.rdf /O:taxonomy_info_pg.txt /Postgres
+rem ..\bin\RDF_Taxonomy_Converter.exe taxonomy_excerpt.rdf /O:taxonomy_info_terms_with_rank.txt /IncludeParents:False /IncludeGrandparents:False /IncludeMnemonic:False /IncludeRank:True
+rem ..\bin\RDF_Taxonomy_Converter.exe taxonomy_excerpt.rdf /O:taxonomy_info_terms_only.txt /IncludeParents:False /IncludeGrandparents:False /IncludeMnemonic:False /IncludeRank:False
 
 echo %date% %time%
