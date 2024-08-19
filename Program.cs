caac45446ef52a609a6e72363a011a52d301751d
alchemistmatt <matthew.monroe@pnnl.gov>
2024-08-18 18:59:06 -0700

    Remove deprecated URL

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ internal static class Program
             ProgramInfo = ConsoleMsgUtils.WrapParagraph("This program processes an RDF taxonomy file and creates a tab-delimited text file with the extracted taxonomy terms"),
             ContactInfo = "Program written by Matthew Monroe for PNNL (Richland, WA)" + Environment.NewLine +
                           "E-mail: matthew.monroe@pnnl.gov or proteomics@pnnl.gov" + Environment.NewLine +
-                          "Website: https://github.com/PNNL-Comp-Mass-Spec/ or https://panomics.pnnl.gov/ or https://www.pnnl.gov/integrative-omics"
+                          "Website: https://github.com/PNNL-Comp-Mass-Spec/ or https://www.pnnl.gov/integrative-omics"
         };
 
         parser.UsageExamples.Add(exeName + " InputFile.rdf /O:OutputFile.txt");
